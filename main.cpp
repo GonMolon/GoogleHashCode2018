4e3eb800014e5ce9f1886f689d452b26065b1a72
Robert <robertplanas87@gmail.com>
2018-03-01 22:02:40 +0100

    update and propose

--- main.cpp
+++ main.cpp
@@ -5,6 +5,15 @@
 
 using namespace std;
 
+
+int R;
+int C;
+int F;
+int N;
+int N_act;
+int B;
+int T;
+
 struct pos {
     int x, y;
 
@@ -31,12 +40,12 @@ public:
 
 struct car {
     pos p;
-    int t;
+    int time;
     vector<int> tasks;
 
     car() {
         p = pos(0, 0);
-        t = 0;
+        time = 0;
     }
 
     void print() {
@@ -47,7 +56,47 @@ struct car {
         cout << endl;
     }
 
+    int update_task(task t) {
+        int D = abs(p.x - t.init.x) + abs(p.y - t.init.y );
+        int Ben = abs(t.init.x - t.end.x) + abs(t.end.y - t.init.y);
+        int Te = max(D,t.s - time);
+        p = t.end;
+        if (time + Te > t.s) {
+            time = Te + time + Ben;
+            return Ben;
+        }
+        else {
+            time = time + Te + Ben;
+            return Ben + B;
+        }
+
+    }
+
     int propose_task(task t) {
+        int D = abs(p.x - t.init.x) + abs(p.y - t.init.y );
+        int Ben = abs(t.init.x - t.end.x) + abs(t.end.y - t.init.y);
+        int Te = max(D,t.s - time);
+        int Bonus = 1;
+        if (Te > D) Bonus = 0;
+
+        //POSSIBLE O NO
+        if (time + D + Ben > t.f) return -1;
+
+        double Dw = Te / (R+C);
+        double Benw = Ben/ (R+C);
+        double Bw = Bonus*B / (Benw*10000);
+
+        double Taskw = N_act / N;
+
+        double c1 = 0.1;
+        double c2 = 0.3;
+        double c3 = 0.3;
+        double c4 = 1- c1 - c2 - c3;
+        double Pref = 0.001;
+        double prob = c1 * Dw + c2 * Benw + c3 * Bw + c4 * Taskw;
+
+        if (prob > Pref) return 1;
+        return 0;
 
     }
 };
@@ -67,12 +116,7 @@ typedef priority_queue<task, vector<task>, comparator> cell;
 
 typedef vector<vector<cell>> matrix;
 
-int R;
-int C;
-int F;
-int N;
-int B;
-int T;
+
 
 matrix m;
 
