cb7b290b3daacc2580e76ed24169da293f742f8d
Gonzalo Solera <gonzalo1796@gmail.com>
2018-03-11 12:55:35 +0100

    Changing completely strategy, redoing the problem from scratch. Getting good results.

--- main.cpp
+++ main.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <queue>
-#include <unordered_set>
+#include <list>
 
 using namespace std;
-
 
 int R;
 int C;
 int F;
-int F_act;
 int N;
 int N_act;
 int B;
 int T;
+int total_assigned_tasks;
 
 struct pos {
     int x, y;
@@ -26,227 +24,158 @@ struct pos {
         this->y = y;
     }
 
-    bool operator==(const pos& p) const {
-        return x == p.x && y == p.y;
-    }
-
-};
-
-struct pos_hasher {
-public:
-    size_t operator()(const pos &p) const {
-        return hash<int>()(p.x) ^ hash<int>()(p.y);
+    int dist(const pos& p) const {
+        return abs(x - p.x) + abs(y - p.y);
     }
 };
+
 
 struct task {
 public:
     pos init;
     pos end;
+    int length;
     int s, f;
-    int id;
+    bool available;
+    int possible_cars;
 
-    task(ifstream& input, int id) {
+    task() {}
+
+    task(ifstream& input) {
         input >> init.x >> init.y >> end.x >> end.y >> s >> f;
-        this->id = id;
+        available = true;
+        possible_cars = F;
+        length = init.dist(end);
+    }
+
+    void rejected_from_car() {
+        if(--possible_cars == 0 && available) {
+            --N_act;
+        }
     }
 };
+
+vector<task> tasks;
 
 struct car {
     pos p;
     int time;
-    vector<int> tasks;
-    queue<pos> cache;
-    unordered_set<pos, pos_hasher> used;
-    bool deleted;
+    list<int> available_tasks;
+    list<int> assigned_tasks;
 
     car() {
         p = pos(0, 0);
         time = 0;
-        deleted = false;
+        for(int i = 0; i < N; ++i) {
+            available_tasks.push_back(i);
+        }
     }
 
     void print() {
-        cout << tasks.size() << " ";
-        for(int i = 0; i < tasks.size(); ++i) {
-            cout << tasks[i] << " ";
+        cout << assigned_tasks.size() << " ";
+        for(int id : assigned_tasks) {
+            cout << id << " ";
         }
         cout << endl;
     }
 
-    int update_task(task t) {
-        tasks.push_back(t.id);
-
-        int D = abs(p.x - t.init.x) + abs(p.y - t.init.y );
-        int Ben = abs(t.init.x - t.end.x) + abs(t.end.y - t.init.y);
-        int Te = max(D,t.s - time);
+    void assign_task(int id) {
+        assigned_tasks.push_back(id);
+        tasks[id].available = false;
+        --N_act;
+        ++total_assigned_tasks;
+        task& t = tasks[id];
+        time = time + p.dist(t.init) + t.length;
         p = t.end;
-        if (time + Te > t.s) {
-            time = Te + time + Ben;
-            return Ben;
-        }
-        else {
-            time = time + Te + Ben;
-            return Ben + B;
-        }
-
     }
 
-    int propose_task(task t) {
-        int D = abs(p.x - t.init.x) + abs(p.y - t.init.y );
-        int Ben = abs(t.init.x - t.end.x) + abs(t.end.y - t.init.y);
-        int Te = max(D,t.s - time);
-        int Bonus = 1;
-        if (Te > D) Bonus = 0;
-
-        //POSSIBLE O NO
-        if (time + D + Ben > t.f) return -1;
-
-        double Dw = Te / (R+C);
-        double Benw = Ben/ (R+C);
-        double Bw = Bonus*B / (Benw*10000);
-
-        double Taskw = N_act / N;
-
-        double c1 = 0.1;
-        double c2 = 0.3;
-        double c3 = 0.3;
-        double c4 = 1- c1 - c2 - c3;
-        double Pref = 0.001;
-        double prob = c1 * Dw + c2 * Benw + c3 * Bw + c4 * Taskw;
-
-        if (prob > Pref) return 1;
-        return 1;
-        return 0;
-
+    int get_score(task& t) {
+        int dist = p.dist(t.init);
+        bool bonus = max(dist, t.s - time) > dist; // Maybe greater equal?
+        return bonus * B + t.length;
     }
-};
 
-class comparator {
-public:
-    bool operator()(task a, task b) {
-        if(a.s != b.s) {
-            return a.s > b.s;
-        } else {
-            return a.f <= b.f;
+    double get_heuristic(task& t) {
+        int dist = p.dist(t.init);
+        return get_score(t) - dist;
+    }
+
+    bool is_reachable(task& t) {
+        int dist = p.dist(t.init);
+        return time + dist + t.length <= t.f;
+    }
+
+    int choose_task() {
+        int best_task = -1;
+        double best_heuristic = 0;
+
+        auto it = available_tasks.begin();
+        while(it != available_tasks.end()) {
+            int id = *it;
+            if(not tasks[id].available || not is_reachable(tasks[id])) {
+                tasks[id].rejected_from_car();
+                it = available_tasks.erase(it);
+            } else {
+                double heuristic = get_heuristic(tasks[id]);
+                if(best_task == -1 || heuristic > best_heuristic) {
+                    best_task = id;
+                    best_heuristic = heuristic;
+                }
+                ++it;
+            }
         }
+
+        return best_task;
     }
 };
 
-typedef priority_queue<task, vector<task>, comparator> cell;
 
-typedef vector<vector<cell>> matrix;
-
-matrix m;
 int score;
-
-vector<car> cars;
+list<car> cars;
+list<car> used_cars;
 
 void read(ifstream& input) {
     input >> R >> C >> F >> N >> B >> T;
     N_act = N;
-    F_act = F;
+    total_assigned_tasks = 0;
     score = 0;
 
-    m = matrix(R, vector<cell>(C));
+    tasks = vector<task>(N);
+    cars = list<car>(F);
 
     for(int i = 0; i < N; ++i) {
-        task t = task(input, i);
-        m[t.init.x][t.init.y].push(t);
+        task t = task(input);
+        tasks[i] = t;
     }
-
-    cars = vector<car>(F);
 }
 
-
 void solve() {
+    while(cars.size() > 0) {
+//        cout << "tasks: " << N_act << endl;
+//        cout << "cars: " << cars.size() << endl;
+        double best_heuristic = 0;
+        list<car>::iterator selected_car = cars.end();
+        int assigned_task = 0;
 
-    while(F_act > 0) {
-        for(int i = 0; i < cars.size() && N_act > 0; ++i) {
-//            cout << i << endl;
-//            cout << N_act << endl
-            cout << F_act << endl;
-
-            car& car = cars[i];
-            if(car.deleted) {
-                break;
-            }
-
-            queue<pos> q;
-            if(!car.cache.empty()) {
-                q = car.cache;
+        auto it = cars.begin();
+        while(it != cars.end()) {
+            int id = it->choose_task();
+            if(id == -1) {
+                used_cars.push_back(*it);
+                it = cars.erase(it);
             } else {
-                q.push(car.p);
-            }
-
-            int dist = 0;
-            int current_level = 1;
-            int next_level = 0;
-
-            bool stop = false;
-
-            while(!q.empty() && !stop) {
-                pos p = q.front();
-//                cout << "current pos: " << p.x << ", " << p.y << endl;
-                q.pop();
-//                cout << "dist: " << dist << endl;
-
-                cell& cell = m[p.x][p.y];
-                if(!cell.empty()) {
-
-                    int res = car.propose_task(cell.top());
-                    if(res == 0) {
-                        car.cache = q;
-                        break;
-                    } else if(res == 1) {
-                        N_act--;
-                        score += car.update_task(cell.top());
-                        cell.pop();
-                    } else { // This task is not feasible, keep looking for another one
-                        // Propose the rest of tasks in this cell
-                        // NOT BY NOW
-                    }
+                double heuristic = it->get_heuristic(tasks[id]);
+                if(selected_car == cars.end() || heuristic > best_heuristic) {
+                    best_heuristic = heuristic;
+                    selected_car = it;
+                    assigned_task = id;
                 }
-
-                for(int r = -1; r <= 1; ++r) {
-                    for(int c = -1; c <= 1; ++c) {
-                        if((r == 0 || c == 0) && (r != 0 || c != 0)) {
-                            pos next = p;
-                            next.x += r;
-                            next.y += c;
-                            if(next.x >= 0 && next.y >= 0 && next.x < R && next.y < C) {
-                                int d = abs(next.x - car.p.x) + abs(next.y - car.p.y );
-                                if(d > dist) {
-                                    pair<int, int> aux(next.x, next.y);
-                                    if(car.used.count(next) == 0) {
-                                        car.used.insert(next);
-                                        q.push(next);
-                                        ++next_level;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-
-
-                if(--current_level == 0) {
-                    current_level = next_level;
-                    next_level = 0;
-                    ++dist;
-
-                    car.used.clear();
-                }
-
+                ++it;
             }
-
-            if(q.empty()) {
-                --F_act;
-                car.deleted = true;
-                car.used.clear();
-                // TODO delete this car since it can't get another task ever
-            }
-
+        }
+        if(selected_car != cars.end()) {
+            selected_car->assign_task(assigned_task);
+            score += selected_car->get_score(tasks[assigned_task]);
         }
     }
 }
@@ -254,10 +183,10 @@ void solve() {
 
 int main(int num_args, char* args[]) {
 
-//    if(num_args != 2) {
-//        cout << "usage: ./main input_file.in" << endl;
-//        exit(1);
-//    }
+    if(num_args != 2) {
+        cout << "usage: ./main input_file.in" << endl;
+        exit(1);
+    }
 
     ifstream input;
     input.open(args[1]);
@@ -266,9 +195,12 @@ int main(int num_args, char* args[]) {
 
     solve();
 
-    cout << "score: " << score << endl;
-
-    for(int i = 0; i < F; ++i) {
-        cars[i].print();
+    for(car c : used_cars) {
+        c.print();
     }
+
+//    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" << endl;
+//    cout << "score: " << score << endl;
+//    cout << "ratio: " << ((double)total_assigned_tasks)/N << endl;
+
 }
